fdd848ff38fb325a68c0d1f82f6003871d9bc615
xkcm <xkcm16@gmail.com>
2023-07-05 01:38:11 +0200

    Minor fix

--- tests/example-errors.ts
+++ tests/example-errors.ts
@@ -9,7 +9,7 @@ import BetterError, {
 export const DEFAULT_METADATA = {
   testValue: "test",
 };
-export const DEFAULT_CODE = "test.fs.empty_file" as string;
+export const DEFAULT_CODE = "test.fs.empty_file";
 export const DEFAULT_MESSAGE = "This is a default message";
 
 @withMetadata(DEFAULT_METADATA)
