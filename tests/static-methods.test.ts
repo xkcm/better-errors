4f73cd0cca45a7eb46dc144fafa682d42b2d7adb
xkcm <xkcm16@gmail.com>
2023-07-05 14:05:56 +0200

    Add unit tests

--- tests/static-methods.test.ts
+++ tests/static-methods.test.ts
@@ -1,6 +1,15 @@
 import { describe, expect, it } from "vitest";
 import { InferMetadata } from "../src";
-import { DEFAULT_METADATA, ErrorWithDefaultMetadata, GenericError } from "./example-errors";
+
+import {
+  DEFAULT_CODE,
+  DEFAULT_MESSAGE,
+  DEFAULT_METADATA,
+  ErrorWithDefaultCode,
+  ErrorWithDefaultMessage,
+  ErrorWithDefaultMetadata,
+  GenericError,
+} from "./example-errors";
 
 describe("Static methods", () => {
   it("should use metadata passed to withMetadata static method", () => {
@@ -27,5 +36,35 @@ describe("Static methods", () => {
     expect(defaultError.metadata).toStrictEqual(DEFAULT_METADATA);
     expect(newError.metadata).toStrictEqual(newMetadata);
     expect(newerError.metadata).toStrictEqual(newerMetadata);
+  });
+
+  it("should use default code from decorator after static method was called", () => {
+    const newCode = "new.code";
+    const newerCode = "newer.code";
+    const ErrorWithNewCode = ErrorWithDefaultCode.withCode(newCode);
+    const ErrorWithNewerCode = ErrorWithNewCode.withCode(newerCode);
+
+    const defaultError = new ErrorWithDefaultCode();
+    const newError = new ErrorWithNewCode();
+    const newerError = new ErrorWithNewerCode();
+
+    expect(defaultError.code).toEqual(DEFAULT_CODE);
+    expect(newError.code).toEqual(newCode);
+    expect(newerError.code).toEqual(newerCode);
+  });
+
+  it("should use default message from decorator after static method was called", () => {
+    const newMessage = "New message";
+    const newerMessage = "Newer message";
+    const ErrorWithNewMessage = ErrorWithDefaultMessage.withMessage(newMessage);
+    const ErrorWithNewerMessage = ErrorWithNewMessage.withMessage(newerMessage);
+
+    const defaultError = new ErrorWithDefaultMessage();
+    const newError = new ErrorWithNewMessage();
+    const newerError = new ErrorWithNewerMessage();
+
+    expect(defaultError.message).toEqual(DEFAULT_MESSAGE);
+    expect(newError.message).toEqual(newMessage);
+    expect(newerError.message).toEqual(newerMessage);
   });
 });
