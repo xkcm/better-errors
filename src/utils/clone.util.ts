46c9359128482eefc7098b9d0a8f351fc54667ac
xkcm <xkcm16@gmail.com>
2023-07-05 15:40:27 +0200

    Fix cloned class name

--- src/utils/clone.util.ts
+++ src/utils/clone.util.ts
@@ -1,5 +1,10 @@
 // eslint-disable-next-line import/prefer-default-export, @typescript-eslint/no-explicit-any
 export function cloneClass<C extends { new (...args: any[]): any }>(OriginalClass: C): C {
   class ClonedClass extends OriginalClass {}
+
+  Reflect.defineProperty(ClonedClass, "name", {
+    value: OriginalClass.name,
+  });
+
   return ClonedClass;
 }
